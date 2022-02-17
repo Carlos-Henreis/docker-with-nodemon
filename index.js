2bbac6f7e1d4fc946f89f6af5f1e532ccc700159
carlos-henreis <carlos_henreis@yahoo.com.br>
2022-02-16 21:06:52 -0300

    teste com nodemon

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const { PORT = '3000' } = process.env
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Aplicativo de exemplo ouvindo na porta ${PORT}`)
+})
